3422a7e809795b14e9c2bc3b09e1ff3713934d52
Daniel Solis Salge <danisolsalge82@gmail.com>
2023-01-16 21:10:11 -0300

    Implementing possible moves of Rook

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public abstract class Piece{
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -16,14 +16,14 @@ public abstract class Piece{
 
     public abstract boolean[][] possibleMoves();
 
-    public boolean possibleMoves(Position position) {
+    public boolean possibleMove(Position position) {
         return possibleMoves()[position.getRow()][position.getColumn()];
     }
 
     public boolean isThereAnyPossibleMove() {
         boolean[][] mat = possibleMoves();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
                 if (mat[i][j]) {
                     return true;
                 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,34 +2,37 @@ package boardgame;
 
 public class Position {
 
-    private Integer row;
-    private Integer column;
+    private int row;
+    private int column;
 
-    public Position(Integer row, Integer column) {
+    public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
-    public Integer getRow() {
+    public int getRow() {
         return row;
     }
 
-    public void setRow(Integer row) {
+    public void setRow(int row) {
         this.row = row;
     }
 
-    public Integer getColumn() {
+    public int getColumn() {
         return column;
     }
 
-    public void setColumn(Integer column) {
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setValues(int row, int column) {
+        this.row = row;
         this.column = column;
     }
 
     @Override
     public String toString() {
-        return row
-                + ", "
-                + column;
+        return row + ", " + column;
     }
 }
